400cf28dd0b51a5e6b93c13a2bd28108c56d8a0a
Frederic Cambus <fred@statdns.com>
2018-10-04 22:42:21 +0200

    Remove the default endpoint, and add 'ip' endpoint

--- src/ip.c
+++ src/ip.c
@@ -1,10 +1,10 @@
 #include <kore/kore.h>
 #include <kore/http.h>
 
-int		page(struct http_request *);
+int		ip(struct http_request *);
 
 int
-page(struct http_request *req)
+ip(struct http_request *req)
 {
 	http_response(req, 200, NULL, 0);
 	return (KORE_RESULT_OK);
