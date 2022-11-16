8c4e770e10541844d5f2c553d4037b1f2cc1d0e1
Frederic Cambus <fred@statdns.com>
2022-11-16 10:34:16 +0100

    Remove extraneous quotation mark characters in payload.

--- payload.go
+++ payload.go
@@ -16,15 +16,15 @@ package main
 
 type payload struct {
 	IP                string  `json:"ip"`
-	Continent         string  `json:"continent_code,omitempty""`
-	Country           string  `json:"country,omitempty""`
-	CountryCode       string  `json:"country_code,omitempty""`
-	CountryCode3      string  `json:"country_code3,omitempty""`
-	IsInEuropeanUnion bool    `json:"is_in_european_union,omitempty""`
-	Region            string  `json:"region,omitempty""`
-	RegionCode        string  `json:"region_code,omitempty""`
-	City              string  `json:"city,omitempty""`
-	PostalCode        string  `json:"postal_code,omitempty""`
+	Continent         string  `json:"continent_code,omitempty"`
+	Country           string  `json:"country,omitempty"`
+	CountryCode       string  `json:"country_code,omitempty"`
+	CountryCode3      string  `json:"country_code3,omitempty"`
+	IsInEuropeanUnion bool    `json:"is_in_european_union,omitempty"`
+	Region            string  `json:"region,omitempty"`
+	RegionCode        string  `json:"region_code,omitempty"`
+	City              string  `json:"city,omitempty"`
+	PostalCode        string  `json:"postal_code,omitempty"`
 	Latitude          float64 `json:"latitude,omitempty"`
 	Longitude         float64 `json:"longitude,omitempty"`
 	TimeZone          string  `json:"timezone,omitempty"`
